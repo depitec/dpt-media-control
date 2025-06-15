fe03f7c376b8de467a04554ccc450419131ea78b
Jan Schmidt <hi@jnsmdt.dev>
2025-06-15 21:32:41 +0200

    feat: added template_config as plain config option

--- install_deps.sh
+++ install_deps.sh
@@ -6,8 +6,16 @@ if [ $USER == "root" ]; then
 	exit
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo "### Installing requirements"
 echo ""
 # run uv as user
 /usr/local/bin/uv sync
 echo ""
+
+echo "### Copy necessary files"
+
+# copy template_config to ~/.config/dpt-media-control/config.toml
+
+cp -r $SCRIPT_DIR/template_config $HOME/.config/dpt-media-control/config.toml
