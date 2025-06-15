70204f31589dd3b70346d7fdcf9164086ddf79af
Jan Schmidt <hi@jnsmdt.dev>
2025-06-15 20:02:04 +0200

    feat: check for root user in install_deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,13 @@
-	echo "### Installing requirements"
-	echo ""
-	# run uv as user
-	/usr/local/bin/uv sync
-	echo ""
+#!/bin/bash
+
+# check for sudo and exit if root user
+if [ $USER == "root" ]; then
+	echo "This script should not be run as root"
+	exit
+fi
+
+echo "### Installing requirements"
+echo ""
+# run uv as user
+/usr/local/bin/uv sync
+echo ""
